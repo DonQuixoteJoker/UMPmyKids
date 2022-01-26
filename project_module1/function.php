799ea7af2d07fe5cb4902bc0fc1feec1cd01303e
LeeZiXuan <92352011+LeeZiXuan@users.noreply.github.com>
2022-01-26 23:15:34 +0800

    Update function.php

--- project_module1/function.php
+++ project_module1/function.php
@@ -34,7 +34,7 @@ session_start();
 					header('location: adminDashboard.php');
 				} else if($userType == "owner") 
                 {
-					header('location: owner_dashboard.php');
+					header('location: adminDashboard.php');
 				} else if($userType == "UMP Staff") 
                 {
 					header('location: adminDashboard.php');
