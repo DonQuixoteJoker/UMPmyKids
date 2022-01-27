379b22db9f09ac90debe61cf17d455bab97cc48e
Muhammad Afif <mr.qwertyuiop981224@gmail.com>
2022-01-27 21:09:09 +0800

    Update dbcon.php

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,16 +1,20 @@
 <?php
-/* Database connection settings */
-session_start();
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$database = 'umpmykids';
-//$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
-$conn=mysqli_connect($host,$user,$pass,$database);
-if($conn){
-}else{
-     echo"Connection not successful" . mysqli_error($conn);
-     die($conn);
-}
+define('DB_TYPE','mysql');
+define('DB_HOST',$cleardb_server);
+define('DB_NAME',$cleardb_db);
+define('DB_USER',$cleardb_username);
+define('DB_PASS',$cleardb_password);
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+session_start();
 ?>
